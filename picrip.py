4b50fe46e164eda4fffbab396e9a4474f9876520
jdavv <davernjeanluc@gmail.com>
2018-10-09 17:12:18 -0500

    did a lot here
    Signed-off-by: jdavv <davernjeanluc@gmail.com>

--- picrip.py
+++ picrip.py
@@ -9,14 +9,14 @@ import praw
 
 
 class PicRip:
-    """
-    Create an instance of of reddit using the PRAW library. This will allow us to get all reddit users submissions
-    easily and go from there with ripping any images
-    """
-    def __init__(self, reddit_bot, username):
 
+    def __init__(self, reddit_bot, username):
+        """
+        :param reddit_bot: string : name of credentials for reddit's API located in the praw.ini file
+        :param username: string : name of reddit user
+        """
         # user praw.ini for client authentication.
-        self.reddit_bot = None
+        self.reddit_bot = reddit_bot
         # instantiate an instance of the reddit object
         self.reddit = praw.Reddit(reddit_bot)
         # known gfycat domain
@@ -50,6 +50,34 @@ class PicRip:
         # mime type for urls that are not images
         self.content_type_further_processing = ['text/html']
 
+        # spin it up on init
+        asyncio.run(self.run())
+
+    async def run(self):
+        """
+        This function is called upon initialization of the PicRip class.
+        Sorts URLs and handles API calls asynchronously to create the class attribute
+        of urls_ready_to_download.
+        :return:
+        """
+        print('Getting posts for {}'.format(self.username))
+        self.get_user_posts()
+
+        tasks = [self.check_url_response(url) for url in self.unprocessed_posts]
+        await asyncio.gather(*tasks, return_exceptions=False)
+
+        domain_check = [self.sort_url_by_domain(url) for url in self.urls_requires_further_processing]
+        await asyncio.gather(*domain_check, return_exceptions=False)
+
+        get_hashes = [self.imgur_get_hash(url) for url in self.imgur_urls]
+        await asyncio.gather(*get_hashes, return_exceptions=False)
+
+        imgur_api_calls = [self.imgur_api_call(album_hash) for album_hash in self.imgur_album_hashes]
+        await asyncio.gather(*imgur_api_calls, return_exceptions=False)
+
+        gfycat_api_calls = [self.gfycat_get_json(url) for url in self.gfycat_urls]
+        await asyncio.gather(*gfycat_api_calls, return_exceptions=False)
+
     def get_user_posts(self):
         """
         Call various methods on the reddit instance to create a list of user's submissions URLs
@@ -65,7 +93,6 @@ class PicRip:
         :param url: string : url that has an imgur domain
         :return: string : 5 or 7 characters long
         """
-
         path = urlparse(url).path
 
         if path.startswith('/a/') and len(path) is 8:
@@ -86,14 +113,14 @@ class PicRip:
         headers = {'Authorization': imgur_client_id}
         # Imgur's API url
         imgur_api_url = 'https://api.imgur.com/3/'
-        # Full path for each album
+        # Combine API URL and the album hash into a URL for Imgur's API
         full_path = '{0}album/{1}/images'.format(imgur_api_url, album_hash)
 
         async with aiohttp.ClientSession(headers=headers) as session:
             async with session.get(full_path) as response:
 
                 if response.status == 200:
-                    return await self.process_imgur_album_json(await response.json())
+                    await self.process_imgur_album_json(await response.json())
 
     async def process_imgur_album_json(self, imgur_json):
         """
@@ -110,7 +137,6 @@ class PicRip:
         :param url: a string that is the submission link
         :return:
         """
-
         async with aiohttp.ClientSession() as session:
 
             try:
@@ -119,38 +145,44 @@ class PicRip:
                     if response.status == 200:
 
                         if response.content_type in self.content_type_ready_to_download:
-                            print(response.content_type, url)
                             self.urls_ready_to_download.append(url)
 
                         if response.content_type in self.content_type_further_processing:
                             self.urls_requires_further_processing.append(url)
-                            print(response.content_type, url)
 
             except aiohttp.ClientConnectionError as exception:
                 return print(exception)
 
-    async def gfycat_api(self, url):
+    async def gfycat_get_json(self, url):
+        """
+        Make request to the gfycat api for json
+        :param url: string : url to gfycat
+        :return: call to gfycat_json_url_to list to process json
+        """
         api_url = 'https://api.gfycat.com/v1/gfycats{}'.format(urlparse(url).path)
 
         async with aiohttp.ClientSession() as session:
             async with session.get(api_url) as response:
 
                 if response.status == 200:
-                    await self.gfycat_json(await response.json())
+                    await self.gfycat_json_url_to_list(await response.json())
 
-    async def gfycat_json(self, json):
-
+    async def gfycat_json_url_to_list(self, json):
+        """
+        Get the value for the mp4Url and append to urls_ready_to_download list.
+        :param json:
+        :return: gfycat_mp4 : string : is appended to urls_ready_to_download list
+        """
         gfycat_mp4 = (json.get('gfyItem').get('mp4Url'))
 
         return self.urls_ready_to_download.append(gfycat_mp4)
 
-    async def split_url_by_domain(self, url):
+    async def sort_url_by_domain(self, url):
         """
-        Check if url domain is in the attribute imgur_domains
+        Check if url domain is in the attribute imgur_domains or attribute gfycat_domain
         :param url: string :
         :return:
         """
-
         domain = urlparse(url).netloc
 
         if domain in self.imgur_domains:
@@ -159,27 +191,32 @@ class PicRip:
         if domain in self.gfycat_domain:
             self.gfycat_urls.append(url)
 
-async def main():
 
-    username = input("what username")
-    test = PicRip(reddit_bot='bot1', username=username)
+def main():
+    """
+    Create objects from the username list
+    :return: call print_all_urls with a list of objects
+    """
+    usernames = []
+    user_objects = []
 
-    test.get_user_posts()
-    tasks = [test.check_url_response(url) for url in test.unprocessed_posts]
-    await asyncio.gather(*tasks, return_exceptions=False)
-    domain_check = [test.split_url_by_domain(url) for url in test.urls_requires_further_processing]
-    await asyncio.gather(*domain_check, return_exceptions=False)
-    # print('imgur albums', test.length_of_list(test.imgur_urls))
-    print(len(test.urls_ready_to_download), 'ready to download')
-    get_hashes = [test.imgur_get_hash(url) for url in test.imgur_urls]
-    await asyncio.gather(*get_hashes, return_exceptions=False)
-    imgur_api_calls = [test.imgur_api_call(album_hash) for album_hash in test.imgur_album_hashes]
-    await asyncio.gather(*imgur_api_calls, return_exceptions=False)
-    print(len(test.urls_ready_to_download), 'urls before gfycat')
-    gfycat_api_calls = [test.gfycat_api(url) for url in test.gfycat_urls]
-    await asyncio.gather(*gfycat_api_calls, return_exceptions=False)
-    print(len(test.urls_ready_to_download), 'urls after gfycat')
-asyncio.run(main())
+    for username in usernames:
+        username = PicRip(reddit_bot='bot1', username=username)
+        user_objects.append(username)
+
+    return print_all_urls(user_objects)
 
 
+def print_all_urls(user_objects):
+    """
+    Iterate through the list attribute urls_ready_to_download from each object
+    :param user_objects: a list of PicRip instances
+    :return: prints url to stdout
+    """
+    for user_object in user_objects:
+        for url in user_object.urls_ready_to_download:
+            print(url)
 
+
+if __name__ == "__main__":
+    main()
